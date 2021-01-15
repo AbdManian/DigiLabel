dc203c8ea19500210090ac7f7146a3f0d43044ad
AbdManian <abd.manian@gmail.com>
2021-01-15 10:18:05 +0330

    Fix issue in name of argument

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import argparse
 parser = argparse.ArgumentParser(description=
                                  'Scale and merge multi-page label pdf files into single A4 PDF')
 
-parser.add_argument('--output', '-o', dest='OutputFile', help='Output pdf file', required=True)
+parser.add_argument('--output', '-o', dest='outputfile', help='Output pdf file', required=True)
 
 parser.add_argument('labels', action='store', nargs='+', help='Input label files')
 
 args = parser.parse_args()
 
-labeler.convert_label_files(args.labels, args.output)
+labeler.convert_label_files(args.labels, args.outputfile)
